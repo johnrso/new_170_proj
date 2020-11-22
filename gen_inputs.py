32a39194045c8fa782a7b8df6869c4b0845b7b25
john so <>
2020-11-21 20:36:12 -0800

    creating groups and giving them nice values

--- gen_inputs.py
+++ gen_inputs.py
@@ -3,14 +3,16 @@ import numpy as np
 import sys
 import os
 import getopt
+from itertools import combinations
 
 def main(argv):
     FP = "./inputs/"
-    n = 0
+    groups = {}
+    budgets = {}
     s = np.random.random() * 100
 
     try:
-        opts, args = getopt.getopt(argv,"n:s",)
+        opts, args = getopt.getopt(argv,"n:g:s:",)
     except getopt.GetoptError:
         print('invalid arguments')
         sys.exit(2)
@@ -18,15 +20,51 @@ def main(argv):
     for opt, arg in opts:
         if opt == "-n":
             n = int(arg)
+        if opt == "-g":
+            spl = arg.split()
+            for g in spl:
+                budgets[g] = 0
+                for i in g:
+                    groups[i] = g;
         if opt == "-s":
             s = float(arg)
+
+    strag = n - len(groups)
+    s_max = (s / (len(budgets) + strag)) - .015
+
+    for g in budgets:
+        budgets[g] = [s_max, len(list(combinations(g, 2))), 0]
+
+    print(s)
+    print(s_max)
+    print(strag)
+    print(groups)
+    print(budgets)
 
     out = "{:d} \n".format(n)
     out += "{:.3f} \n".format(s)
 
     for i in range(n):
         for j in range(i + 1, n):
-            h_ij, s_ij = np.random.random() * 100, np.random.random() * 100
+            s_ij = 0
+
+            if str(i) in groups and str(j) in groups[str(i)]:
+                # out += " * "
+                curr, num, _ = budgets[groups[str(i)]]
+                curr = float(curr)
+                if num == 1:
+                    s_ij = curr
+                else:
+                    s_ij = np.random.random() * curr
+
+                budgets[groups[str(i)]][0] -= s_ij
+                budgets[groups[str(i)]][1] -= 1
+                budgets[groups[str(i)]][2] += s_ij
+
+            else:
+                s_ij = np.random.random() * 100
+
+            h_ij = np.random.random() * 100
             out += "{:d} {:d} {:.3f} {:.3f} \n".format(i, j, h_ij, s_ij)
 
     if not os.path.exists(FP):
