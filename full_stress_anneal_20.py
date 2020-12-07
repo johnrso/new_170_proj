bcf45433adcc2af89fbe8d96621a71ee0076c62c
Michael Zhu <mbzhu1@gmail.com>
2020-12-06 22:03:04 -0800

    new change

--- full_stress_anneal_20.py
+++ full_stress_anneal_20.py
@@ -12,36 +12,50 @@ from utils import *
 from parse import *
 from os.path import *
 
-ITERATIONS = 10 ** 5
+from tqdm import tqdm
 
-def anneal_solve_20(G, s):
+ITERATIONS =  1 * 10 ** 2
+
+def anneal_solve_20(G, s, D_prev, h_prev):
     curr = {}
     rooms = {}
 
-    best_happiness = 0
+    best_happiness = h_prev
 
-    for i in range(20):
-        curr[i] = 0
+    # print(type(D_prev))
+    # print(D_prev)
 
-    if is_valid_solution(curr, G, s, 1):
-        return (curr, 1)
+    curr = D_prev
 
-    for i in range(20):
-        curr[i] = i
-        rooms[i] = [i]
+    # for i in D_prev.keys():
+    #     curr[i] = D_prev[i]
 
-    best = (dict(curr), 20)
+    # if is_valid_solution(curr, G, s, 1):
+    #     return (curr, 1)
+
+    # for i in range(20):
+    #     curr[i] = i
+    #     rooms[i] = [i]
+
+    for node, room in curr.items():
+        if not room in rooms.keys():
+            rooms[room] = []
+        rooms[room].append(node)
+
+    best = (dict(curr), len(rooms))
 
     G_copy = G.copy()
     for e in list(G_copy.edges.data()):
-        if e[2]['stress'] > s / 2:
+        if e[2]['stress'] > s / len(rooms):
+            G_copy.remove_edge(*e[:2])
+        elif e[2]['happiness'] == 0:
             G_copy.remove_edge(*e[:2])
 
-    T = 100000
+    T = 100     
 
     # start = timeit.default_timer()
 
-    for i in range(ITERATIONS):
+    for i in tqdm(range(ITERATIONS)):
         # if i % 100 == 0:
         #     end = timeit.default_timer()
         #     print("{} out of {}, elapsed time: {}".format(i, ITERATIONS, end - start))
@@ -49,7 +63,8 @@ def anneal_solve_20(G, s):
 
         st1 = random.choice(range(20))
         poss_swaps = G_copy.edges(st1)
-        st2 = random.choice(list(poss_swaps))[1]
+        lps = list(poss_swaps)  
+        st2 = random.choice(lps)[1]
 
         st1_num, st2_num = curr[st1], curr[st2]
         st1_room, st2_room = rooms[st1_num], rooms[st2_num]
@@ -116,30 +131,57 @@ def reorder_rooms(rooms):
 #     write_output_file(D, 'out/test.out')
 
 if __name__ == '__main__':
-    inputs = glob.glob('./all_inputs/medium-*')
+    userin = sys.argv[1]
+    inputs = glob.glob('./all_inputs/medium-{}.in'.format(userin))
     ttl = len(inputs)
     ct = 0
+    improvement_count = 0
+    total_happiness_improvement = 0
+    for i in range(100):
+        for input_path in inputs:
+            # if(input_path == "./tempin/medium-128.in"):
+            #     try:
+            #         output_path = './all_outputs_mz/' + basename(normpath(input_path))[:-3] + '.out'
+            #         D_o = read_output_file(output_path, G, s)
+            #         print(input_path)
+            #         print(D_o)
+            #         h_o = calculate_happiness(D_o, G)
+            #         print(h_o)
+            #     except Exception as e:
+            #         print(e)
+            #     continue
+            if ct % 50 == 0:
+                print("{} out of {}".format(ct, ttl))
+            print(input_path)
 
-    for input_path in inputs:
-        if ct % 50 == 0:
-            print("{} out of {}".format(ct, ttl))
+            output_path = './all_outputs_mz/' + basename(normpath(input_path))[:-3] + '.out'
 
-        output_path = './all_outputs/' + basename(normpath(input_path))[:-3] + '.out'
+            G, s = read_input_file(input_path, 20)
 
-        G, s = read_input_file(input_path, 20)
+            try:
+                D_o = read_output_file(output_path, G, s)
+            except Exception as e:
+                print(e)
+            h_o = calculate_happiness(D_o, G)
+            
+            try:
+                D, k = anneal_solve_20(G, s, D_o, h_o)
+                G, s = read_input_file(input_path, 20)
+                assert is_valid_solution(D, G, s, k)
+            except:
+                print("counldn't do annealing")
 
-        D_o = read_output_file(output_path, G, s)
-        h_o = calculate_happiness(D_o, G)
+            h = calculate_happiness(D, G)
 
-        D, k = anneal_solve_20(G, s)
-        G, s = read_input_file(input_path, 20)
-        assert is_valid_solution(D, G, s, k)
+            print(h, h_o)
 
-        h = calculate_happiness(D, G)
+            if h > h_o:
+                print("improvement on {} ({} vs {}), overwriting...".format(input_path, D_o, D))
+                write_output_file(D, output_path)
+                improvement_count+=1
+                total_happiness_improvement += h - h_o
 
-        print(h, h_o)
-
-        if h > h_o:
-            print("improvement on {} ({} vs {}), overwriting...".format(input_path, D_o, D))
-        write_output_file(D, output_path)
-        ct += 1
+            ct += 1
+        ct = 0
+    print("improvements made: {}".format(improvement_count))
+    print("total increased happiness score: {}".format(total_happiness_improvement))
